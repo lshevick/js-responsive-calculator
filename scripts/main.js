268fff918fae1affc640aa7985443defadbef40c
Levi Shevick <levidshevick@gmail.com>
2022-05-05 14:28:47 -0400

    adds pushOperator and calculate function

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,49 @@
     const $calculator = document.querySelector('.calculator');
     const $screen = document.querySelector('.calculator-screen');
     const $numbers = document.querySelectorAll('.number');
-    const $operator = document. querySelector('.operator');
+    const $operators = document. querySelectorAll('.operator');
+    const $clear = document.querySelector('.clear');
+    const $plusMinus = document.querySelector('.plus-minus');
+    const $percent = document.querySelector('.percent');
+    const $equals = document.querySelector('.equal-sign');
+
     let i = 0;
+
+    pushNumber();
+    pushOperator();
+    calculate();
+
     
+    $plusMinus.addEventListener('click', () => {
+        console.log(`${$plusMinus.value}`);
+    })
+    $percent.addEventListener('click', () => {
+        console.log(`${$percent.value}`);
+    })    
+
+    function calculate() {
+        $equals.addEventListener('click', () => {
+            console.log(`${$equals.innerHTML}`);
+        })
+    }
+   
+    function pushOperator() {
+    $clear.addEventListener('click', () => {
+        console.log(`${$clear.innerHTML}`);
+    })
+
+    for (let i = 0; i < $operators.length; i++){
+        $operators[i].addEventListener('click', () => {
+            console.log(`${$operators[i].value}`);
+        });
+        }
+    };
+    
+    function pushNumber() {
     for (let i = 0; i < $numbers.length; i++){
-        $numbers[i].addEventListener('click', pushNumber);
-        
-        function pushNumber() {
+        $numbers[i].addEventListener('click', () => { 
             console.log(`${$numbers[i].value}`);
+        });
         }
     };
 
