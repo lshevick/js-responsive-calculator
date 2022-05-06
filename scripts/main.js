983224fa0ccd2811d69d8004ec980a9a46b73974
Levi Shevick <levidshevick@gmail.com>
2022-05-06 12:48:41 -0400

    cleaned up code in calculate function

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-(function() {
+(function () {
     'use strict';
 
     const $calculator = document.querySelector('.calculator');
     const $screen = document.querySelector('.calculator-screen');
     const $numbers = document.querySelectorAll('.number');
-    const $operators = document. querySelectorAll('.operator');
+    const $operators = document.querySelectorAll('.operator');
     const $clear = document.querySelector('.clear');
     const $plusMinus = document.querySelector('.plus-minus');
     const $percent = document.querySelector('.percent');
@@ -16,11 +16,11 @@
     let calculation = [];
     let result = 0;
     let operator;
-    
-    
+
+
     pushNumber();
     pushOperator();
-        
+
     $equals.onclick = function () {
         calculation.push(prevNum);
         calculation.flat();
@@ -30,72 +30,66 @@
         switch (operator) {
             case '+':
                 result = parseFloat(num2) + parseFloat(num1);
-                prevNum = [];
-                prevNum.push(result);
                 break;
             case '-':
                 result = parseFloat(num2) - parseFloat(num1);
-                prevNum = [];
-                prevNum.push(result);
                 break;
             case '/':
                 result = parseFloat(num2) / parseFloat(num1);
-                prevNum = [];
-                prevNum.push(result);
                 break;
             case '*':
                 result = parseFloat(num2) * parseFloat(num1);
-                prevNum = [];
-                prevNum.push(result);
                 break;
-            default: 
+            default:
                 result = currentNum;
-            }
-            console.log(calculation);
+        }
+        prevNum = [];
+        prevNum.push(result);
+        console.log(calculation);
         console.log(result);
         $screen.value = result;
         currentNum = [];
         calculation = [];
     }
 
-        function pushOperator() {
-           
-            $clear.onclick = () => {
-                prevNum = [];
-                currentNum = [];
-                result = [];
-                calculation = [];
-                $screen.value = 0;
-                console.clear();
-                console.log('Clean slate');
-            }
+    function pushOperator() {
 
-
-            for (let i = 0; i < $operators.length; i++) {
-                $operators[i].onclick = () => {
-                        operator = $operators[i].value;
-                        currentNum = prevNum;
-                        prevNum = [];
-                        console.log('changed Nums!');
-                        calculation.push(currentNum, $operators[i].value);
-                        console.log($operators[i].value, 'pushed to calc');
-                }
-            }
-        };
-        
-        function pushNumber() {
-            for (let i = 0; i < $numbers.length; i++){
-                $numbers[i].onclick = () => { 
-                    prevNum.push($numbers[i].value);
-                    $screen.value = prevNum.join('');
-                    // if (result) {
-                    //     result = 0;
-                    //     $screen.value = 0;
-                    // };
-            console.log(currentNum);
-            console.log(prevNum);
+        $clear.onclick = () => {
+            prevNum = [];
+            currentNum = [];
+            result = [];
+            calculation = [];
+            $screen.value = 0;
+            console.clear();
+            console.log('Clean slate');
         }
-    }
+
+
+        for (let i = 0; i < $operators.length; i++) {
+            $operators[i].onclick = () => {
+                operator = $operators[i].value;
+                currentNum = prevNum;
+                prevNum = [];
+                console.log('changed Nums!');
+                calculation.push(currentNum, $operators[i].value);
+                console.log($operators[i].value, 'pushed to calc');
+            }
+        }
+    };
+
+    function pushNumber() {
+        for (let i = 0; i < $numbers.length; i++) {
+            $numbers[i].onclick = () => {
+                prevNum.push($numbers[i].value);
+                $screen.value = prevNum.join('');
+                // if (result) {
+                //     result = 0;
+                //     $screen.value = 0;
+                // };
+                console.log(currentNum);
+                console.log(prevNum);
+            }
+        }
     };
 
 })();
